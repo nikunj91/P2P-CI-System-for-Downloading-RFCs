2814fdc210e960779ea1454c871eeb8fa346ef2b
nikunj91 <shah_nikunj91@hotmail.com>
2016-04-11 18:52:03 -0400

    Basic Client Server Connect using TCP Socket Programming

--- client.py
+++ client.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python           # This is client.py file
+
+import socket
+serverName = 'localhost'
+serverPort = 12000
+clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+sentence = raw_input('Input lowercase sentence:')
+clientSocket.send(sentence)
+modifiedSentence = clientSocket.recv(1024)
+print 'From Server:',modifiedSentence
+clientSocket.close()
--- server.py
+++ server.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python           # This is server.py file
 
+import socket
+serverPort = 12000
+serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+serverSocket.bind(('',serverPort))
+serverSocket.listen(1)
+print 'The server is ready to receive'
+while 1:
+    connectionSocket, addr = serverSocket.accept()
+    sentence = connectionSocket.recv(2048)
+    capitalizedSentece = sentence.upper()
+    connectionSocket.send(capitalizedSentece)
+    connectionSocket.close()
