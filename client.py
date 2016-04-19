a2b3f491d653678e6db312ab833e76d75b1b101a
nikunj91 <shah_nikunj91@hotmail.com>
2016-04-19 04:05:33 -0400

    Inserted comments in the code

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python           
-# This is the client
+# This is the client file code
 #Author Nikunj Shah
 #Author Aparna Patil
 
@@ -13,80 +13,84 @@ import time
 import email.utils
 
 # Server Information
-serverPort = 7734
-serverName = socket.gethostname()
-print "serverName "+serverName
 
-client_RFC_list = {}	# The RFCs that the client has currently
+#Server IP Address from command line
+serverPort = int(sys.argv[1])
+#Server Port from command line
+serverName = sys.argv[2]
 
-# Connecting to Server
+# Will store the RFCs that the client has currently at the time of coming up
+client_RFC_list = {}	
+
+# Connecting to Server using Socket and TCP
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
+print 'Connected to server at address: '+serverName+" and port: "+serverPort
 
-# Making RFC list
-#rfcs = [f for f in os.listdir(os.getcwd()+'/RFC/')]
-
+#Create the ADD Request message
 def create_add_request(client_rfc_num,client_rfc_title):
-
 	message = "ADD RFC "+str(client_rfc_num)+" P2P-CI/1.0\r\n"\
 			  "Host: "+str(client_hostname)+"\r\n"\
 			  "Port: "+str(upload_client_port_number)+"\r\n"\
 			  "Title: "+str(client_rfc_title)+"\r\n"\
-
-	print message
-
 	return message
 
+#Create the LOOKUP Request message
 def create_lookup_request(client_rfc_num, client_rfc_title):
-
 	message = "LOOKUP RFC "+str(client_rfc_num)+" P2P-CI/1.0\r\n"\
 			  "Host: "+str(client_hostname)+"\r\n"\
 			  "Port: "+str(upload_client_port_number)+"\r\n"\
 			  "Title: "+str(client_rfc_title)+"\r\n"\
-
-	print message
 	return message
 
+#Create the GET Request message
 def create_get_request(client_rfc_num):
-
 	message = "GET RFC "+str(client_rfc_num)+" P2P-CI/1.0\r\n"\
 			  "Host: "+str(client_hostname)+"\r\n"\
 			  "OS: "+platform.platform()+"\r\n"\
-
-	print message
 	return message
 
+#Create the LIST Request message
 def create_list_request():
-
 	message = "LIST ALL P2P-CI/1.0\r\n"\
 			  "Host: "+str(client_hostname)+"\r\n"\
 			  "Port: "+str(upload_client_port_number)+"\r\n"\
-
-	print message
-
 	return message
 
+#This will keep the client upload socket on the randomly generated port number.
+#This upload server socket is always running to listen for incoming file upload requests from other peers
+#It will accept the request
+#If the request is proper then it replies with the required RFC file data
 def upload_thread():
+	
+	#Create a upload server socket
 	uploadSocket = socket.socket()
 	host=socket.gethostname()
 	uploadSocket.bind((host,upload_client_port_number))
 	uploadSocket.listen(5)
 	while 1:
-		print "Upload Thread started"
+		#Accept an incoming request for upload
 		downloadSocket,downloadAddress = uploadSocket.accept()
 		message = downloadSocket.recv(1024)
-		print message
 		split_data=message.split('\r\n')
+		
+		#Check for BAD REQUEST case
 		if len(split_data)==4 and "GET RFC " in split_data[0] and "Host: " in split_data[1] and "OS: " in split_data[2]:
+			#Check for VERSION NOT SUPPORTED case
 			if 'P2P-CI/1.0' in split_data[0]:
+				#Else reply to the request with the requested file data
 				request=split_data[0].split(" ")
 				if request[0]=='GET':
+
+					#Get the request RFC number
 					rfc_number=request[2]
-					print rfc_number
-					rfc_file_path = os.getcwd()+"/RFC2/RFC"+rfc_number+".txt"
-					print rfc_file_path
+
+					#Locate the file and get its data
+					rfc_file_path = os.getcwd()+"/RFC/RFC"+rfc_number+".txt"
 					opened_file = open(rfc_file_path,'r')
 					data = opened_file.read()
+
+					#Formulate the reply to be sent to the requesting peer
 					reply_message = "P2P-CI/1.0 200 OK\r\n"\
 							  "Date: "+str(email.utils.formatdate(usegmt=True))+"\r\n"\
 							  "OS: "+str(platform.platform())+"\r\n"\
@@ -94,7 +98,8 @@ def upload_thread():
 							  "Content-Length: "+str(len(data))+"\r\n"\
 							  "Content-Type: text/plain\r\n"
 					reply_message=reply_message+data
-					print reply_message
+					
+					#Send the reply
 					downloadSocket.sendall(reply_message)
 			else:
 				reply_message="505 P2P-CI Version Not Supported\r\n"
@@ -103,116 +108,185 @@ def upload_thread():
 			reply_message="400 Bad Request\r\n"
 			downloadSocket.send(reply_message)
 
+#Does all the handling for sending the GET request and receiving the RFC file from it 
 def download_rfc_thread(req_message,peer_host_name,peer_port_number,rfc_number):
+
+	#Connect to the upload server socket of the peer holding the required file
 	requestPeerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	requestPeerSocket.connect((peer_host_name,int(peer_port_number)))
+	
 	print 'Connection with peer established'
+
+	#Send the GET request to the peer
 	requestPeerSocket.sendall(req_message)
-	print 'message sent'
+	
+	#Receive the reply from the peer with the data in it.
 	get_reply=""
 	get_reply=requestPeerSocket.recv(1024)
 	if 'P2P-CI/1.0 200 OK' in get_reply.split("\r\n")[0]:
 		print 'P2P-CI/1.0 200 OK'
-		print 'File Received'
 		content_line=(get_reply.split("\r\n"))[4]
 		content_length=int(content_line[content_line.find('Content-Length: ')+16:])
-		print content_length
 		get_reply=get_reply+requestPeerSocket.recv(content_length)
-		#print get_reply
-		#print 'yaaaay'
-		rfc_file_path = os.getcwd()+"/RFC2/RFC"+rfc_number+".txt"
-		print rfc_file_path
-		#print '----------------------------------------------'
-		#print get_reply
+		rfc_file_path = os.getcwd()+"/RFC/RFC"+rfc_number+".txt"
 		data=get_reply[get_reply.find('text/plain\r\n')+12:]
-		#print '----------------------------------------------'
-		#print data
+
+		#Writing the file data to a file
 		with open(rfc_file_path,'w') as file:
 			file.write(data)
-	else:
-		print "505 P2P-CI Version Not Supported\r\n"
-	requestPeerSocket.close()
-	#file.write("xxxxxxxx")
+		print 'File Received from peer and stored locally now'
+	elif 'Version Not Supported' in get_reply.split("\r\n")[0]:
+		print get_reply
+	elif 'Bad Request' in get_reply.split("\r\n")[0]:
+		print get_reply
 
+	#Closing the socket connection
+	requestPeerSocket.close()
+
+# This function will do all code handling for GET,ADD,LOOKUP,LIST and EXIT
 def user_input():
+
+	#Get the raw input from user
 	print "Enter if you want to: ADD, GET, LIST, LOOKUP or EXIT:"
 	service = raw_input()
 
 	if service == "ADD":
+
 		print "Enter RFC Number"
 		client_rfc_num = raw_input()
 		print "Enter Title"
 		client_rfc_title = raw_input()
+
+		#Create the ADD Request message and send it to server using the socket
 		req_message = create_add_request(client_rfc_num,client_rfc_title)
+
+		print "ADD Request to be sent to the server"
+		print req_message
+
 		information_list = [req_message]
 		info_add = pickle.dumps(information_list,-1)
 		clientSocket.send(info_add)
+		
+		#Receive the response from server and print the same
 		response_received = clientSocket.recv(1024)
+		print "ADD Response sent from the server"
 		print response_received
+
 		user_input()
 
 	elif service == "GET":
+
 		print "Enter RFC Number"
 		client_rfc_num = raw_input()
 		print "Enter Title"
 		client_rfc_title = raw_input()
-		print client_rfc_num
-		print client_rfc_title
+
+		#Create a LOOKUP request to find the peer holding the requesting RFC. Send it to the server
 		req_message = create_lookup_request(client_rfc_num, client_rfc_title)
+
+		print "LOOKUP Request to be sent to the server for completing the GET request"
+		print req_message
+
 		information_list = [req_message]
 		info_add = pickle.dumps(information_list, -1)
 		clientSocket.sendall(info_add)
+
+		#Receive the LOOKUP response from the server
 		response_received = clientSocket.recv(1024)
+
+		#Based on server response, verify the response for FILE NOT FOUND, BAD REQUEST or WRONG VERSION
 		split_data=response_received.split('\r\n')
+
+		print "LOOKUP Response sent from the server"
+		
 		if '404 Not Found' in split_data[0]:
 			print response_received
 		elif 'Version Not Supported' in split_data[0]:
 			print response_received
+		elif 'Bad Request' in split_data[0]:
+			print response_received
 		else:
 			print response_received
+
+			#Retrieve the HOSTNAME and PORT NUMBER of the first peer in the response holding the RFC file
 			split_data=split_data[1].split(" ")
 			peer_host_name=split_data[3]
 			peer_port_number=split_data[4]
-			print peer_host_name
-			print peer_port_number
+
+			#Create the GET request to be sent to the peer
 			req_message = create_get_request(client_rfc_num)
+			
+			print "GET Request to be sent to the peer holding the RFC File"
 			print req_message
+
+			#start a new thread that will do all the handling for sending the GET request and receiving the RFC file from it
 			start_new_thread(download_rfc_thread,(req_message,peer_host_name,peer_port_number,client_rfc_num))
 
 		user_input()
 
 	elif service == "LIST":
+
+		#Create a LIST request to be sent to the server
 		req_message = create_list_request()
+
+		print "LIST Request to be sent to the server"
+		print req_message
+
+		#Send the request to the server
 		information_list = [req_message]
 		info_add = pickle.dumps(information_list,-1)
 		clientSocket.send(info_add)
+
+		#Receive the response from the server
 		response_received = clientSocket.recv(1024)
+
+		print "LIST Response sent from the server"
 		print response_received
+
 		user_input()
 
 	elif service == "LOOKUP":
+
 		print "Enter RFC Number"
 		client_rfc_num = raw_input()
 		print "Enter Title"
 		client_rfc_title = raw_input()
+
+		#Create a LOOKUP request to be sent to the server
 		req_message = create_lookup_request(client_rfc_num, client_rfc_title)
+
+		print "LOOKUP Request to be sent to the server"
+		print req_message
+
+		#Send the request to the server
 		information_list = [req_message]
 		info_add = pickle.dumps(information_list, -1)
 		clientSocket.send(info_add)
+
+		#Receive the response from the server
 		response_received = clientSocket.recv(1024)
+
+		print "LOOKUP Response sent from the server"		
 		print response_received
+
 		user_input()
 
 	elif service == "EXIT":
+		#Handle the case when the user wants to stop the client by sending EXIT command
+		#The information will be relayed to the server
+		#The server will then remove information related to all the files that this client had
+		#The client socket connection to the server is closed
 		message = "EXIT"
 		exit_message=pickle.dumps([message],-1)
 		clientSocket.send(exit_message)
 		clientSocket.close()
 
+#Generating the initial peer file data from the specified directory
+#Returns 2 lists, one each for RFC numbers and RFC titles
 def get_rfc_details():
 	rfc_numbers=[]
 	rfc_titles=[]
-	rfc_storage_path = os.getcwd()+"/RFC2"
+	rfc_storage_path = os.getcwd()+"/RFC"
 	for file_name in os.listdir(rfc_storage_path):
 		if 'RFC' in file_name:
 			rfc_number=file_name[file_name.find("C")+1:file_name.find(".")]
@@ -221,18 +295,22 @@ def get_rfc_details():
 			rfc_titles.append(rfc_title)
 	return rfc_numbers,rfc_titles
 
-def send_peer_info(client_hostname):
+#Generating the initial peer file data to send over to the server
+def send_peer_info():
 	rfc_numbers,rfc_titles=get_rfc_details()
-	return [upload_client_port_number,rfc_numbers,rfc_titles,client_hostname]
+	return [upload_client_port_number,rfc_numbers,rfc_titles]
 
+#Generating the random client port number it will use for uploading
 upload_client_port_number = 60000 + random.randint(1,1000)
-client_hostname = socket.gethostname()#+str(upload_client_port_number)
-print "client_hostname "+client_hostname
 
-send_data=send_peer_info(client_hostname)
+#Generating the initial peer file data to send over to the server and sending the same to server
+send_data=send_peer_info()
 data = pickle.dumps(send_data)
 clientSocket.send(data)
 clientSocket.close
+
+#Starts a new thread that maintain the upload server socket over the random upload port generated
 start_new_thread(upload_thread,())
+# This function will do all code handling for GET,ADD,LOOKUP,LIST and EXIT
 user_input()
 
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python           
-# This is the server
+# This is the server file code
 #Author Nikunj Shah
 #Author Aparna Patil
 
@@ -10,22 +10,32 @@ from thread import *
 import platform
 import time
 
+#Starting the TCP server socket 
 serverPort = 7734
 serverName = socket.gethostname()
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind((serverName,serverPort))
-
 serverSocket.listen(5)
 
 print 'The server is ready and up'
 
-peer_info_dict={}   #key:hostname value:upload_port_number
-peer_rfc_dict={} #key: rfc_number value:list of host_names
-rfc_number_title_dict={} #key:rfc_number value:rfc_title
+#Data Structures maintaing the peer data
 
-def insert_data_in_dict(initial_data):
+#key:ip_value 
+#value:upload_port_number
+peer_info_dict={}
+
+#key: rfc_number 
+#value:list of host_names
+peer_rfc_dict={}
+
+#key:rfc_number 
+#value:rfc_title
+rfc_number_title_dict={}
+
+# Insert the intial data into the data structures
+def insert_data_in_dict(initial_data,hostname):
 	global peer_info_dict,peer_rfc_dict
-	hostname=initial_data[3]+":"+str(initial_data[0])
 	peer_info_dict[hostname]=initial_data[0]
 	rfc_list=[]
 	for i in range(0,len(initial_data[1])):
@@ -35,11 +45,8 @@ def insert_data_in_dict(initial_data):
 		else:
 			peer_rfc_dict[initial_data[1][i]]=[hostname]
 		rfc_number_title_dict[initial_data[1][i]]=initial_data[2][i]
-		print [initial_data[1][i],initial_data[2][i]]
-	
-	print peer_rfc_dict
-	print rfc_number_title_dict
 
+#Add the RFC to the data structures
 def add_peer_rfc(rfc_number,rfc_title,client_host_name):
 	global peer_rfc_dict,rfc_number_title_dict
 	if not rfc_number_title_dict.has_key(rfc_number):
@@ -49,9 +56,8 @@ def add_peer_rfc(rfc_number,rfc_title,client_host_name):
 		rfc_host_list=peer_rfc_dict.get(rfc_number)
 		rfc_host_list.append(client_host_name)
 		peer_rfc_dict[rfc_number]=rfc_host_list
-	print rfc_number_title_dict
-	print peer_rfc_dict
 
+#Looksup the RFC and forms the response having the information of the peers holding it
 def lookup_peer(rfc_number,rfc_title,client_host_name,client_port_number):
 	global peer_rfc_dict,rfc_number_title_dict
 	if rfc_number_title_dict.has_key(rfc_number) and rfc_number_title_dict[rfc_number]==rfc_title:
@@ -65,6 +71,7 @@ def lookup_peer(rfc_number,rfc_title,client_host_name,client_port_number):
 		message = "P2P-CI/1.0 404 Not Found\r\n"
 	return message
 
+#Creates a list of all the RFC and forms the response with the information of the peers holding it
 def list_peer(client_host_name):
 	global peer_rfc_dict,rfc_number_title_dict
 	rfc_list=peer_rfc_dict.keys()
@@ -80,33 +87,47 @@ def list_peer(client_host_name):
 		message=message+"\r\n"
 	return message
 
+#Main server thread function that handles all the requests for ADD< LOOKUP, LIST and EXIT and responds to them
 def client_init(connectionsocket, addr):
-	#global active_peer_dict, index_dict, rfc_title_dict
-	print('Got connection from ', addr)
+	
+	#Get the initial data sent by client containing its stored RFC infomation 
 	initial_data = pickle.loads(connectionsocket.recv(1024))
-	print initial_data[0]
-	print initial_data[1]
-	print initial_data[2]
-	print initial_data[3]
-	host_name=initial_data[3]+":"+str(initial_data[0])
-	insert_data_in_dict(initial_data)
+	host_name=addr[0]+":"+str(initial_data[0])
+
+	#Insert the information into data structures
+	insert_data_in_dict(initial_data,host_name)
 
 	while 1:
+		#Request received from client for a service
 		message_received = connectionsocket.recv(1024)
 		message_list = pickle.loads(message_received)
+
+		print "Request received from the client"
+		print message_list[0]
+
+		#EXIT Request
 		if message_list[0][0]== 'E':
 			break
-		#if type(message_list) == str:
+
+		#ADD Request
 		elif message_list[0][0] == 'A':
 			split_data=message_list[0].split('\r\n')
+			#Check for BAD REQUEST case
 			if len(split_data)==5 and "ADD RFC " in split_data[0] and "Host: " in split_data[1] and "Port: " in split_data[2] and "Title: " in split_data[3]:
+				#Check for VERSION NOT SUPPORTED case
+				#If proper then add the data from the request and echo back the same request with OK Response
 				if 'P2P-CI/1.0' in split_data[0]:
+					#Retrieve the newly added RFC file information from request
 					rfc_number=split_data[0][split_data[0].find("C ")+2:split_data[0].find(" P")]
 					client_host_name=split_data[1][split_data[1].find("Host: ")+6:]
 					client_port_number=split_data[2][split_data[2].find("Port: ")+6:]
 					rfc_title=split_data[3][split_data[3].find("Title: ")+7:]
 					p2p_version=split_data[0][split_data[0].find(" P")+1:]
+					
+					#Add the RFC file information into the data structures
 					add_peer_rfc(rfc_number,rfc_title,client_host_name+":"+client_port_number)
+					
+					#Echo back the request with OK response to the client
 					message = "P2P-CI/1.0 200 OK\r\n"+split_data[1]+"\r\n"+split_data[2]+"\r\n"+split_data[3]+"\r\n"
 					connectionsocket.send(message)
 				else:
@@ -116,15 +137,20 @@ def client_init(connectionsocket, addr):
 				message="400 Bad Request\r\n"
 				connectionsocket.send(message)
 
+		#LIST Request
 		elif message_list[0][0] == "L":
 			if message_list[0][1] == "I":
 				split_data=message_list[0].split('\r\n')
-				print len(split_data)
+				#Check for BAD REQUEST case
 				if len(split_data)==4 and "LIST ALL " in split_data[0] and "Host: " in split_data[1] and "Port: " in split_data[2]:
 					p2p_version=split_data[0][split_data[0].find(" P")+1:]
+					#Check for VERSION NOT SUPPORTED case
+					#If proper then reply with the list of RFC and their peer information
 					if p2p_version=='P2P-CI/1.0':
+						#Retrieve information from request
 						client_host_name=split_data[1][split_data[1].find("Host: ")+6:]
 						client_port_number=split_data[2][split_data[2].find("Port: ")+6:]
+						#Reply to the request by sending the response to client
 						message=list_peer(client_host_name+":"+client_port_number)
 						connectionsocket.send(message)
 					else:
@@ -133,20 +159,22 @@ def client_init(connectionsocket, addr):
 				else:
 					message="400 Bad Request\r\n"
 					connectionsocket.send(message)
+
+			#LOOKUP Request
 			else:
 				split_data=message_list[0].split('\r\n')
+				#Check for BAD REQUEST case
 				if len(split_data)==5 and "LOOKUP RFC " in split_data[0] and "Host: " in split_data[1] and "Port: " in split_data[2] and "Title: " in split_data[3]:
-					rfc_number=split_data[0][split_data[0].find("C ")+2:split_data[0].find(" P")]
-					client_host_name=split_data[1][split_data[1].find("Host: ")+6:]
-					client_port_number=split_data[2][split_data[2].find("Port: ")+6:]
-					rfc_title=split_data[3][split_data[3].find("Title: ")+7:]
 					p2p_version=split_data[0][split_data[0].find(" P")+1:]
-					print rfc_number
-					print client_host_name
-					print client_port_number
-					print rfc_title
-					print p2p_version
+					#Check for VERSION NOT SUPPORTED case
+					#If proper then reply with the requested RFC file information
 					if p2p_version=='P2P-CI/1.0':
+						#Retrieve requested RFC file information from request
+						rfc_number=split_data[0][split_data[0].find("C ")+2:split_data[0].find(" P")]
+						client_host_name=split_data[1][split_data[1].find("Host: ")+6:]
+						client_port_number=split_data[2][split_data[2].find("Port: ")+6:]
+						rfc_title=split_data[3][split_data[3].find("Title: ")+7:]
+						#Reply to the request by sending the RFC file information response to client
 						message=lookup_peer(rfc_number,rfc_title,client_host_name+":"+client_port_number,client_port_number)
 						connectionsocket.send(message)
 					else:
@@ -155,7 +183,8 @@ def client_init(connectionsocket, addr):
 				else:
 					message="400 Bad Request\r\n"
 					connectionsocket.send(message)
-		
+	
+	#When the client EXITS, remove all the information for that client from the data structures maintained
 	if peer_info_dict.has_key(host_name):
 		peer_info_dict.pop(host_name,None)
 	rfc_list=peer_rfc_dict.keys()
@@ -166,17 +195,17 @@ def client_init(connectionsocket, addr):
 				rfc_number_title_dict.pop(rfc,None)
 				peer_rfc_dict.pop(rfc,None)
 			else:
-				print host_name
 				rfc_host_list.remove(host_name)
 				peer_rfc_dict[rfc]=rfc_host_list
-
-	print peer_info_dict
-	print peer_rfc_dict
-	print rfc_number_title_dict
-
+	#Close the connection thread for that client
 	connectionsocket.close()
 
+#Keep the server socket always up and running
 while 1:
+	#Receive incoming connection request from client
 	connectionsocket, addr = serverSocket.accept()
+	print('Got incoming connection request from ', addr)
+	#Thread will handle all the incoming requests from clients and respond back to them
 	start_new_thread(client_init, (connectionsocket, addr))
+
 serverSocket.close()
